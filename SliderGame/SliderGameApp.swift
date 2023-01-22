e1783aba9f86507af92e7cffa723a71c5d384fd8
ILKA777 <iomorin@edu.hse.ru>
2023-01-22 12:05:19 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Илья Морин on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
